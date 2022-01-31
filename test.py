ba38071255b6d5222fa8da3f03cf0d7a39c09071
VihaanS22 <69805124+VihaanS22@users.noreply.github.com>
2022-01-31 21:07:31 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split as tts
+from sklearn.model_selection import train_test_split as ttsplit
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import fetch_openml
 from PIL import Image
@@ -38,6 +38,53 @@ if(recap == "yes"):
         classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         nclasses = len(classes)
 
+        x_train, x_test, y_train, y_test = ttsplit(X, Y, random_state = 0, train_size = 7500, test_size = 2500)
+
+        scaled_xtr = x_train/255.0
+        scaled_xts = x_test/255.0
+
+        classifier = LogisticRegression(solver = "saga", multi_class = "multinomial").fit(scaled_xtr, y_train)
+
+        y_pred = classifier.predict(scaled_xts)
+
+        print("Accuracy is :", accuracy_score(y_test, y_pred))
+
+
+        cap = cv2.VideoCapture(0)
+
+        while True:
+
+            ret,frame = cap.read()
+            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            height, width = gray.shape
+            upper_left = (int(width/2-56), int(height/2-56))
+            down_right = (int(width/2+56), int(height/2+56))
+            cv2.rectangle(gray, upper_left, down_right, (0,255,0), 2)
+            roi = gray[upper_left[1] : down_right[1], upper_left[0] : down_right[0]]
+
+            img = Image.fromarray(roi)
+            imgbw = img.convert('L')
+            resized = imgbw.resize((22,30), Image.ANTIALIAS)
+
+            invert = PIL.ImageOps.invert(resized)
+
+            min_pix = np.percentile(invert, 20)
+            scaled = np.clip(invert-min_pix, 0, 255)
+            max_pix = np.max(invert)
+            scaled = np.asarray(scaled)/max_pix
+
+            test = np.array(scaled).reshape(1, 660)
+            test_pred = classifier.predict(test)
+            print("PREDICTED ALPHABET IS :", test_pred)
+            
+            cv2.imshow("Prediction", gray)
+            
+            if cv2.waitKey(1) and 0xFF == ord("q"):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
     elif(recap == "end"):
         print("Goodbye!")
 
@@ -52,7 +99,6 @@ elif(recap == "no"):
     #print(pd.Series(Y).value_counts())
     classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     nclasses = len(classes)
-    #print(classes)
 
     x_train, x_test, y_train, y_test = tts(X, Y, random_state = 0, train_size = 7500, test_size = 2500)
 
@@ -61,41 +107,44 @@ elif(recap == "no"):
 
     classifier = LogisticRegression(solver = "saga", multi_class = "multinomial").fit(scaled_xtr, y_train)
 
+    y_pred = classifier.predict(scaled_xts)
+
+    print("Accuracy is :", accuracy_score(y_test, y_pred))
+
+
     cap = cv2.VideoCapture(0)
 
     while True:
-        try:
-            ret,frame = cap.read()
-            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            height, width = gray.shape
-            upper_left = (int(width/2-56), int(height/2-56))
-            down_right = (int(width/2+56), int(height/2+56))
-            cv2.rectangle(gray, upper_left, down_right, (0,255,0), 2)
-            roi = gray[upper_left[1] : down_right[1], upper_left[0] : down_right[0]]
 
-            img = Image.fromarray(roi)
-            imgbw = img.convert('L')
-            resized = imgbw.resize((28,28), Image.ANTIALIAS)
+        ret,frame = cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        height, width = gray.shape
+        upper_left = (int(width/2-56), int(height/2-56))
+        down_right = (int(width/2+56), int(height/2+56))
+        cv2.rectangle(gray, upper_left, down_right, (0,255,0), 2)
+        roi = gray[upper_left[1] : down_right[1], upper_left[0] : down_right[0]]
 
-            invert = PIL.ImageOps.invert(resized)
+        img = Image.fromarray(roi)
+        imgbw = img.convert('L')
+        resized = imgbw.resize((22,30), Image.ANTIALIAS)
 
-            min_pix = np.percentile(invert, 20)
-            scaled = np.clip(invert-min_pix, 0, 255)
-            max_pix = np.max(invert)
-            scaled = np.asarray(scaled)/max_pix
+        invert = PIL.ImageOps.invert(resized)
 
-            test = np.array(scaled).reshape(1, 784)
-            test_pred = classifier.predict(test)
-            print("PREDICTED ALPHABET IS :", test_pred)
-            
-            cv2.imshow("FRAME", gray)
-            if cv2.waitKey(1) and 0xFF == ord("q"):
-                break
+        min_pix = np.percentile(invert, 20)
+        scaled = np.clip(invert-min_pix, 0, 255)
+        max_pix = np.max(invert)
+        scaled = np.asarray(scaled)/max_pix
 
-
-        except Exception as e:
-            pass
+        test = np.array(scaled).reshape(1, 660)
+        test_pred = classifier.predict(test)
+        print("PREDICTED ALPHABET IS :", test_pred)
+        
+        cv2.imshow("Prediction", gray)
+        
+        if cv2.waitKey(1) and 0xFF == ord("q"):
+            break
 
     cap.release()
     cv2.destroyAllWindows()
+
 
